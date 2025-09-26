868bd35bd0d7f2f3319a6b425f071c9f9c33f3bb
natanaelssr <natanael2ilva@gmail.com>
2025-09-26 14:51:21 -0300

    Atualiza Client ID do Android para 742861794909-je4328bkkcvj6ahsq6ac98piquveb6nl.apps.googleusercontent.com

--- src/config/googleAuth.js
+++ src/config/googleAuth.js
@@ -2,5 +2,5 @@ export const GOOGLE_CLIENT_IDS = {
   // Substitua pelos novos Client IDs do seu projeto Firebase "Agenda Familiar"
   WEB: '742861794909-je4328bkkcvj6ahsq6ac98piquveb6nl.apps.googleusercontent.com',
   IOS: '742861794909-2bmiu7tgo0tngbjfhtj31dudssjtkgpe.apps.googleusercontent.com',
-  ANDROID: '742861794909-tahq3a9dtqro4ls1g9jb92a3flok45nq.apps.googleusercontent.com',
+  ANDROID: '742861794909-je4328bkkcvj6ahsq6ac98piquveb6nl.apps.googleusercontent.com',
 };
